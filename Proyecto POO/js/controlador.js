24e46cd468723d51051854754a4094f0a1e703c6
MauriRamos19 <maury.zavala99@gmail.com>
2020-07-26 20:45:52 -0600

    primer commit

---
+++ Proyecto POO/js/controlador.js
@@ -0,0 +1,535 @@
+var users = [
+	{
+		name: 'Johan Lieberth',
+		email: 'johan19@gmail.com',
+		password: 'asdfg',
+		car:[
+
+		]
+	},
+	{
+		name: 'Arturia Pendragon',
+		email: 'arturia22@gmail.com',
+		password: 'asdfg',
+		car:[
+
+		]
+	},
+	{
+		name: 'Zeke Jeaguer',
+		email: 'zeke20199@gmail.com',
+		password: 'asdfg',
+		car:[
+
+		]
+	},
+	{
+		name: 'Urabe Mikoto',
+		email: 'urabemikoto123@gmail.com',
+		password: 'asdfg',
+		car:[
+
+		]
+	}
+];
+
+var departments = [
+	{
+		nameDepartment: 'Videojuegos',
+		products: [
+			{
+				name: 'The Last of Us Part II - PlayStation 4',
+				brand: 'PlayStation',
+				image: 'img/products/videogames/71p8G+YeA6L._AC_UY218_.jpg',
+				price: '$59.99',
+				calification: 4
+
+			},
+			{
+				name: 'Minecraft - Nintendo Switch ',
+				brand: 'Microsoft',
+				image: 'img/products/videogames/71dIHv1zh7L._AC_UY218_.jpg',
+				price: '$43.26',
+				calification: 5
+			},
+			{
+				name:  'PlayStation 4 Slim 1TB Console',
+				brand: 'PlayStation',
+				image: 'img/products/videogames/índice.webp',
+				price: '$369.01',
+				calification: 4
+			},
+			{
+				name: 'Super Smash Bros - Ultimate (Nintendo Switch)',
+				brand: 'Nintendo',
+				image: 'img/products/videogames/81aJ-R4E6gL._AC_UY218_.jpg',
+				price: '$62.47',
+				calification: 5
+			},
+			{
+				name: 'Nintendo Switch 32GB Console Video Games',
+				brand: 'Nintendo',
+				image: 'img/products/videogames/417kf2GJBuL._AC_US218_.jpg',
+				price: '$435.00',
+				calification: 5
+			}
+		]
+		
+	},
+	{
+		nameDepartment: 'Computadoras',
+		products: [
+			{
+				name: 'AMD Ryzen 5 3600 6-Core, 12-Thread Unlocked Desktop Processor with Wraith Stealth Cooler',
+				brand: 'AMD',
+				image: 'img/products/computers/41m+krxXiBL._AC_US160_.jpg',
+				price: '$159.98',
+				calification: 4
+			},
+			{
+				name: 'HP VH240a 23.8-inch Full HD 1080p IPS LED Monitor with Built-in Speakers and VESA Mounting, Rotating Portrait & Landscape, Tilt, and HDMI & VGA Ports (1KL30AA) - Black',
+				brand: 'HP',
+				image: 'img/products/computers/71trhuzbhML._AC_UY218_.jpg',
+				price: '$59.99',
+				calification: 5
+			},
+			{
+				name: 'SanDisk 128GB Extreme microSDXC UHS-I Memory Card with Adapter - C10, U3, V30, 4K, A2, Micro SD - SDSQXA1-128G-GN6MA ',
+				brand: 'SandDisk',
+				image: 'img/products/computers/61Ck0CO5+HL._AC_UY218_.jpg',
+				price: '$59.99',
+				calification: 4
+			},
+			{
+				name: 'Intel Core i9-9900K Desktop Processor 8 Cores up to 5.0 GHz Turbo unlocked LGA1151 300 Series 95W',
+				brand: 'Intel',
+				image: 'img/products/computers/71RsweT83eL._AC_UL135_SR135,135_.jpg',
+				price: '$449.99',
+				calification: 5
+			},
+			{
+				name: 'ASUS VivoBook 15 Thin and Light Laptop, 15.6” FHD Display, Intel i3-1005G1 CPU, 8GB RAM, 128GB SSD',
+				brand: 'ASUS',
+				image: 'img/products/computers/81fstJkUlaL._AC_UY218_.jpg',
+				price: '$394.00',
+				calification: 5
+			}
+
+		]
+		
+	},
+	{
+		nameDepartment: 'Electronicos',
+		products: [
+			{
+				name: 'TOZO T10 Bluetooth 5.0 Wireless Earbuds with Wireless Charging Case IPX8 Waterproof TWS Stereo Headphones in Ear Built in Mic Headset Premium Sound with Deep Bass for Sport Black',
+				brand: 'TOZO',
+				image: 'img/products/electronics/71isyD36rbL._AC_UY218_.jpg',
+				price: '$36.99',
+				calification: 4
+
+			},
+			{
+				name: 'Shure SRH940 Professional Reference Headphones (Silver)',
+				brand: 'Shure',
+				image: 'img/products/electronics/81Q5ud1jT5L._AC_UY218_.jpg',
+				price: '$249.99',
+				calification: 3
+			},
+			{
+				name:  'TCL 32S325 32 Inch 720p Roku Smart LED TV (2019)',
+				brand: 'TCL',
+				image: 'img/products/electronics/71YZlXXFktL._AC_UY218_.jpg',
+				price: '$127.99',
+				calification: 4
+			},
+			{
+				name: 'Casio FX115ESPLUS Scientific Calculator',
+				brand: 'Nintedo',
+				image: 'img/products/electronics/61sUgi4lgpL._AC_UY218_.jpg',
+				price: '$16.70',
+				calification: 4
+			},
+			{
+				name: 'Garmin 010-01689-00 Forerunner 35; Easy-to-Use GPS Running Watch, Black',
+				brand: 'Garmin',
+				image: 'img/products/electronics/71wamdtiUHL._AC_UY218_.jpg',
+				price: '$99.00',
+				calification: 5
+			}
+
+		]
+		
+	},
+	{
+		nameDepartment: 'Moda par hombres',
+		products: [
+			{
+				name: "Nike Men's Gymnastics Shoes",
+				brand: 'Nike',
+				image: 'img/products/moda para hombres/81uiWMk9dnL._AC_UL320_.jpg',
+				price: '$96.53',
+				calification: 4
+
+			},
+			{
+				name: "Lucky Brand Men's Venice Burnout V Neck Tee",
+				brand: 'Microsoft',
+				image: 'img/products/moda para hombres/A1mEmA7vWVL._AC_UL320_.jpg',
+				price: '$19.91',
+				calification: 5
+			},
+			{
+				name:  "Van Heusen Men's Shirt Regular Fit Poplin Solid",
+				brand: 'Van Heusen',
+				image: 'img/products/moda para hombres/61pjj7DGsTL._AC_UL320_.jpg',
+				price: '15.99',
+				calification: 4
+			},
+			{
+				name: "BITLIVE Men's Cotton Linen Plus Size Stretchy Waist Casual Ankle Length Pants",
+				brand: 'BITLIVE',
+				image: 'img/products/moda para hombres/51Mkq70jqcL._AC_UL320_.jpg',
+				price: '$26.99',
+				calification: 5
+			},
+			{
+				name: 'SaphiRose Hooded Rain Poncho Waterproof Raincoat Jacket for Men Women Adults',
+				brand: 'SaphiRose',
+				image: 'img/products/moda para hombres/51bylErfJQL._AC_UL320_.jpg',
+				price: '$19.99',
+				calification: 5
+			}
+
+		]
+		
+	},
+
+	{
+		nameDepartment: 'Moda para mujeres',
+		products: [
+			{
+				name: "Teva Women's Original Universal Sandal ",
+				brand: 'Teva',
+				image: 'img/products/moda para mujeres/713wPY+4+uL._AC_UL320_.jpg',
+				price: '$41.67',
+				calification: 4
+
+			},
+			{
+				name: "Gentle Souls by Kenneth Cole Women's Nyssa Ruffle Strap Wedge Platform Pump",
+				brand: 'Gentle Souls',
+				image: 'img/products/moda para mujeres/61uUg-v86SL._AC_UL320_.jpg',
+				price: '$82.81',
+				calification: 5
+			},
+			{
+				name:  "Simple Flavor Women's Floral Vintage Dress Elegant Midi Evening Dress",
+				brand: 'Simple Flavor',
+				image: 'img/products/moda para mujeres/71tOqSpSShL._AC_UL320_.jpg',
+				price: '$23.00',
+				calification: 4
+			},
+			{
+				name: "QIXING Women's Summer Casual Loose Mini Dress V-Neck Bell Short Sleeve Shift Dress",
+				brand: 'QIXING',
+				image: 'img/products/moda para mujeres/61CSrx8j4iL._AC_UL320_.jpg',
+				price: '$28.99',
+				calification: 5
+			},
+			{
+				name: 'AOWEER Womens High Waisted Cargo Pants Pockets Casual Loose Combat Twill Trousers Girls',
+				brand: 'AOWEER',
+				image: 'img/products/moda para mujeres/51opLA92djL._AC_UL320_.jpg',
+				price: '$29.99',
+				calification: 5
+			}
+
+		]
+		
+	},
+	{
+		nameDepartment: 'Salud y casa',
+		products: [
+			{
+				name: 'Disposable Gloves, Squish Clear Vinyl Gloves Latex Free Powder-Free Glove PVC Cleaning Health Gloves for Kitchen Cooking Cleaning Safety Food Handling, 100PCS/Box, X-Large，Ship from USA',
+				brand: 'Squish',
+				image: 'img/products/health and household/515I19pV1JL._AC_UL320_.jpg',
+				price: '$20.99',
+				calification: 3
+
+			},
+			{
+				name: 'Listerine Total Care Anticavity Mouthwash, 6 Benefit Fluoride Mouthwash for Bad Breath and Enamel Strength, Fresh Mint Flavor, 1 L ',
+				brand: 'Listerine',
+				image: 'img/products/health and household/81vNiROdiIL._AC_UL320_.jpg',
+				price: '$6.57',
+				calification: 4
+			},
+			{
+				name:  "Philips Norelco MG7750/49 Multigroom Series 7000, Men's Grooming Kit with Trimmer for Beard, Head, Body, and Face - No Blade Oil Needed",
+				brand: 'Philips Norelco',
+				image: 'img/products/health and household/71YCllvavVL._AC_UL320_.jpg',
+				price: '$54.91',
+				calification: 4
+			},
+			{
+				name: 'MainBasics Bath Shower Loofah Sponge Pouf Body Scrubber Exfoliator',
+				brand: 'MainBasics',
+				image: 'img/products/health and household/9140SX9HAGL._AC_UL320_.jpg',
+				price: '$6.99',
+				calification: 4
+			},
+			{
+				name: "Vitamin C by Nature's Bounty, Immune Support, Vitamin C 500mg, 250 Tablets",
+				brand: "Nature's Bounty",
+				image: 'img/products/health and household/71tecTdG6BL._AC_UL320_.jpg',
+				price: '$8.26',
+				calification: 4
+			}
+
+		]
+		
+	}
+	
+
+];
+
+
+
+var localStorage = window.localStorage
+
+if (localStorage.getItem('users') == null) {
+	localStorage.setItem('users', JSON.stringify(users));
+} else {
+	users = JSON.parse(localStorage.getItem('users'))
+}
+
+if (localStorage.getItem('departments') == null) {
+	localStorage.setItem('departments', JSON.stringify(departments));
+} else {
+	products = JSON.parse(localStorage.getItem('departments'))
+}
+
+
+
+
+
+	
+
+
+departments.forEach((department,i) => {
+	document.getElementById('list-departments').innerHTML +=
+		`<option id="nameDepartment" value="${department.nameDepartment}">${department.nameDepartment}</option>`;
+});
+
+
+users.forEach((user,i) => {
+	document.getElementById('list-users').innerHTML +=
+		`<option id="${i}" value="${user.name}">${user.name}</option>`
+});
+
+
+
+
+function registerUser () {
+	var user = {
+		name: document.getElementById('user-name').value,
+		email: document.getElementById('user-email').value,
+		password: document.getElementById('user-password').value,
+		car: []
+	};
+
+	
+	users.push(user);
+	localStorage.setItem('users', JSON.stringify(users));
+	
+
+}
+
+
+
+
+function productsDepartment () {
+	document.getElementById('products').innerHTML = '' 
+	departments.forEach((department,i) => {
+		
+		if (document.getElementById('list-departments').value == department.nameDepartment) {
+			
+			department.products.forEach((product,k) => {
+				
+				var estrellas = '';
+				for(let j=0; j<product.calification; j++){
+					estrellas += '<i class="fas fa-star"></i>';
+				}
+				for(let j=0; j<5-product.calification; j++){
+					estrellas += '<i class="far fa-star"></i>';
+				}
+				document.getElementById('products').innerHTML += 
+					`<div class="col-12" id="prod${k}">
+					<div class="card my-3 mx-auto" style="max-width: 1000px;">
+					<div class="row no-gutters">
+					  <div class="col-md-4" style="background-color: #EAEDED;">
+						<img src="${product.image}" data-toggle="modal" data-target="#productModal${k}" onclick="productsModal(${i},${k})" class="card-img" alt="...">
+					  </div>
+					  <div class="col-md-8">
+						<div class="card-body">
+						  <h5 class="card-title" data-toggle="modal" data-target="#productModal${k}" onclick="productsModal(${i},${k})">${product.name}</h5>
+						  <div>
+						  	${estrellas}
+						  </div>
+						  <div class="mt-5">
+							<strong>${product.price}</strong>
+						  </div>
+						</div>
+					  </div>
+					</div>
+				  </div>
+				  <hr>
+					</div>
+					`
+			});
+			
+		};
+	});
+	
+}
+
+productsDepartment();
+
+
+function productsModal (i,k) {
+	document.getElementById('products-modal').innerHTML = ''
+	departments[i].products.forEach(prod =>{
+		prod = departments[i].products[k]
+		var estrellas = '';
+		for(let j=0; j<prod.calification; j++){
+			estrellas += '<i class="fas fa-star"></i>';
+		}
+		for(let j=0; j<5-prod.calification; j++){
+			estrellas += '<i class="far fa-star"></i>';
+		}
+		document.getElementById('products-modal').innerHTML += 
+		`
+		<div class="modal fade h-5" id="productModal${k}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog modal-lg modal-dialog-centered">
+			<div class="modal-content">
+				
+				<div class="modal-body">
+					<div class="row">
+						<div class="col-12 col-md-6">
+							<img src="${prod.image}" width=180px" alt="">
+						</div>
+						<div class="col-12 col-md-6">
+							<div>
+								<strong>${prod.name}</strong></br>
+							<div>
+							<div class="pt-2">
+								<p><strong>Marca:</strong> ${prod.brand}</p></br>
+							<div>
+							<div>
+								${estrellas}
+							</div>
+							<div class="mt-5">
+								<strong>${prod.price}</strong>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary" onclick="agregarProducto(${i},${k})"><i style="color:black;" class="fas fa-shopping-cart"></i> Agregar al carrito</button>
+				</div>
+			</div>
+			</div>
+		</div>`
+
+
+	});
+	
+	
+}
+
+
+function agregarProducto (i,k) { 
+	var prod = departments[i].products[k];
+	users.forEach((user,k) => {
+		if(document.getElementById('list-users').value == user.name){
+			var product = {
+				name: `${prod.name}`,
+				brand: `${prod.brand}`,
+				price: `${prod.price}`
+			}
+			
+			user.car.push(product);
+			carritoModal(k);
+			localStorage.setItem('users',JSON.stringify(users));
+		};
+
+	});
+	
+	$(`productModal${k}`).modal('hide');
+	
+}
+
+
+
+
+function changeUser() {
+    var nameUsuario;
+    var indice;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].name == document.getElementById('list-users').value) {
+            indice = i;
+            break;
+        }   
+    };
+    
+    carritoModal(indice);
+    
+}
+changeUser();
+
+
+function carritoModal (indice) {
+	document.getElementById('productsUser').innerHTML = '';
+	users[indice].car.forEach((prod,i) => {
+		document.getElementById('productsUser').innerHTML +=
+				`<div class="col-12">
+					<div class="row">
+						<div class="col-3">
+							${i+1}${'.'}
+						</div>
+						<div class="col-3">
+						 	${prod.name}
+						</div>
+						<div class="col-3">
+							${prod.brand}
+						</div>
+						<div class="col-3">
+							${prod.price}
+						</div>
+					</div>
+					<hr>
+				</div>`
+
+	});
+			
+	
+}
+
+
+function deleteProduct () {
+	var index = document.getElementById('indexDelete').value-1
+	
+	for (let i = 0; i < users.length; i++) {
+		if (document.getElementById('list-users').value == users[i].name) {
+			users[i].car.splice(index,1)
+			carritoModal(i)
+			break;
+		}
+		
+	}
+	$('productsUser').modal('hide');
+}
+
